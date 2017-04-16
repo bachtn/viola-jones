7fc98c8e1864e50842e33a05761e677d3fc84e84
Alaa Bakhti <alaa.bakhti@epita.fr>
2017-04-17 00:23:24 +0200

    [DATA][SPLIT][TEST] split data into training and testing depending on a tarin_percentage. Tested added methods

--- src/data.py
+++ src/data.py
@@ -2,6 +2,7 @@
 
 import operator
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 class Data(object):
     """
@@ -9,17 +10,19 @@ class Data(object):
     The data is characterized by:
     * list of attributes / features (length, width, ..)
     * list of classes / labels((dog / cat ...), (Binary classification: True / False), ... )
-    * data (values and their corresponding class (features + labels)
-      Eg:  h = 3, w = 5, class = dog)
-    * training_data -> data for training
-    * testing_data -> data for testing
+    * data (2d matrix containing the values of the features) Eg:  h = 3, w = 5
+    * targets (1d array containing the labels (classes) of the data) Eg: 'dog'
+    * train_percent = the percentage of the data used for training
+        Eg: 0.8 -> 80% of data used for training and 20% used for testing
     """
 
-    def __init__(self, features, labels, data):
+    def __init__(self, features, labels, data, targets, train_percent):
         self.features = features
         self.labels = labels
         self.__data = data
-        #(self.training_data, self.testing_data) = Data.categorizeData(self.data)
+        self.__targets = targets
+        self.train_percent = train_percent
+        (self.xtrain, self.xtest, self.ytrain, self.ytest) = Data.categorizeData(self.__data, self.__targets, self.train_percent)
 
     features = property(operator.attrgetter('_features'))
 
@@ -32,10 +35,10 @@ class Data(object):
     labels = property(operator.attrgetter('_labels'))
 
     @labels.setter
-    def labels(self, labels):
-        if (not labels or not isinstance(labels, list) or len(labels) < 2):
+    def labels(self, value):
+        if (not value or not isinstance(value, list) or len(value) < 2):
             raise ValueError("labels should be a list containing a minimum of 2 elements")
-        self._labels = labels
+        self._labels = value
 
     __data = property(operator.attrgetter('_data'))
 
@@ -43,10 +46,34 @@ class Data(object):
     def __data(self, data):
         # data.shape give (a, b) where a = nbr columns, b = nbr rows
         if (not isinstance(data, np.ndarray) or data.size == 0
-            or (data.shape[1] != len(self.features) + 1)):
+            or (data.shape[1] != len(self.features))):
             raise ValueError("Data should be presented as a 2d non empty numpy array" +
-                    " with n columns where n = nbr of features + 1")
+                    " with n columns where n = nbr of features")
         self._data = data
 
-    def categorizeData(data):
-        pass
+    __targets = property(operator.attrgetter('_targets'))
+
+    @__targets.setter
+    def __targets(self, targets):
+        if (not isinstance(targets, np.ndarray) or targets.size != self.__data.shape[0]
+            or (targets.ndim != 1)):
+            raise ValueError("Targets should be presented as a 1d non empty numpy array" +
+                    " with 1 column and an equal size to the data matrix")
+        self._targets = targets
+
+    train_percent = property(operator.attrgetter('_train_percent'))
+
+    @train_percent.setter
+    def train_percent(self, train_percent):
+        x = not train_percent or not isinstance(train_percent, float) or train_percent < 0.5 or train_percent > 0.9
+        if (not train_percent or not isinstance(train_percent, float) or train_percent < 0.5 or train_percent > 0.9):
+            raise ValueError("Train percentage should be a non empty float between 0.5 and 0.9")
+        self._train_percent = train_percent
+
+    def categorizeData(data, targets, train_percent):
+        xtrain, xtest, ytrain, ytest = train_test_split(data, targets, train_size=train_percent)
+        return (xtrain, xtest, ytrain, ytest)
+
+    def __str__(self):
+        return ("features = %s\nlabels = %s\ndata = %s\ntargets = %s\ntrain_percent = %s"
+                % (self.features, self.labels, self.__data, self.__targets, self.train_percent))
--- tests/dataTest.py
+++ tests/dataTest.py
@@ -10,7 +10,12 @@ class DataTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        pass
+        features = ['height', 'width']
+        labels = ['cat', 'dog']
+        data = np.arange(10).reshape((5,2))
+        targets = np.array([0,1,1,0,1])
+        train_percent = 0.8
+        self.data = Data(features, labels, data, targets, train_percent)
 
     @classmethod
     def tearDownClass(self):
@@ -20,27 +25,55 @@ class DataTest(unittest.TestCase):
         # Empty arguments
         featuresArg = ['height', 'width']
         labelsArg = ['dog', 'cat']
-        dataArg = np.zeros(shape=(4, 3))
+        dataArg = np.zeros(shape=(4, 2))
+        targetsArg = np.array([0,1,1,0])
+        train_percent_arg = 0.8
         with self.assertRaises(TypeError): Data()
         with self.assertRaises(TypeError): Data(featuresArg)
         with self.assertRaises(TypeError): Data(featuresArg, labelsArg)
+        with self.assertRaises(TypeError): Data(featuresArg, labelsArg, dataArg)
+        with self.assertRaises(TypeError): Data(featuresArg, labelsArg, dataArg, targetsArg)
+
         # Should not fail
         try:
-            Data(featuresArg, labelsArg, dataArg)
+            Data(featuresArg, labelsArg, dataArg, targetsArg, train_percent_arg)
         except Exception as e:
             self.fail("Constructor raises exception: %s, while it should not" % type(e).__name__)
+        trainPercents = [0.5, 0.9, 0.8]
+        try:
+            for valid_train_percent_arg in trainPercents:
+                Data(features=featuresArg, labels=labelsArg, data=dataArg, targets=targetsArg, train_percent=valid_train_percent_arg)
+        except Exception as e:
+            self.fail("Constructor raises exception: %s, while it should not" % type(e).__name__)
+
         # Invalid arguments
+        ## features
         with self.assertRaises(ValueError):
-            Data(features=None, labels=labelsArg, data=dataArg)
+            Data(features=None, labels=labelsArg, data=dataArg, targets=targetsArg, train_percent=train_percent_arg)
         labelsInvalidArgs = [None, [1]]
-        for labelsArg in labelsInvalidArgs:
+
+        ## labels
+        for invalidLabelsArg in labelsInvalidArgs:
             with self.assertRaises(ValueError):
-                Data(features=featuresArg, labels=labelsArg, data=dataArg)
-        dataInvalidArgs = [None, [[0]*3]*4, np.zeros(shape=(4,1)),
-                np.zeros(shape=(4,2)), np.zeros(shape=(0,3))]
-        for dataArg in dataInvalidArgs:
+                Data(features=featuresArg, labels=invalidLabelsArg, data=dataArg, targets=targetsArg, train_percent=train_percent_arg)
+
+        ## data
+        dataInvalidArgs = [None, [[0]*3]*4, np.zeros(shape=(4,1)), np.zeros(shape=(0,3))]
+        for invalidDataArg in dataInvalidArgs:
             with self.assertRaises(ValueError):
-                Data(features=featuresArg, labels=labelsArg, data=dataArg)
+                Data(features=featuresArg, labels=labelsArg, data=invalidDataArg, targets=targetsArg, train_percent=train_percent_arg)
+
+        ## targets
+        targetsInvalidArgs = [None, [[0]*3]*4, np.zeros(shape=(4,1)), np.zeros(shape=(4,2)), np.zeros(shape=(0,3))]
+        for invalidTargetsArg in targetsInvalidArgs:
+            with self.assertRaises(ValueError):
+                Data(features=featuresArg, labels=labelsArg, data=dataArg, targets=invalidTargetsArg, train_percent=train_percent_arg)
+
+        ## train_percent
+        trainPercentInvalidArgs = [0., 0.4, 0.95, -0.2, -2., 5., 1., 100.]
+        for invalid_train_percent_arg in trainPercentInvalidArgs:
+            with self.assertRaises(ValueError):
+                Data(features=featuresArg, labels=labelsArg, data=dataArg, targets=targetsArg, train_percent=invalid_train_percent_arg)
 
 
 
